46e2e98225116e8486888fc696d02e53857dfe05
kabhishek2023@gmail.com <kabhishek2023@gmail.com>
2023-03-13 11:16:55 +0530

    completed the Register controller

--- server/controller/appController.js
+++ server/controller/appController.js
@@ -1,3 +1,5 @@
+import UserModel from '../model/User.model.js';
+import bcrypt from 'bcrypt';
 
 
 
@@ -13,8 +15,60 @@
   "profile": ""
 }
 */
-export async function register(req,res){
-    res.json('register route');
+export async function register(req, res) {
+    try {
+        const { username, password, profile, email } = req.body;
+
+        // check the existing username
+        const existUsername = new Promise((resolve, reject) => {
+            UserModel.findOne({ username }).then( user => {
+                // if (err) reject(new Error(err))
+                if (user) reject({ error: "Please use unique username" });
+
+                resolve();
+            })
+        });
+        // check the existing Email
+        const existEmail = new Promise((resolve, reject) => {
+            UserModel.findOne({ email }).then( email => {
+                // if (err) reject(new Error(err))
+                if (email) reject({ error: "Please use unique Email" });
+
+                resolve();
+            })
+        });
+
+        Promise.all([existUsername, existEmail])
+            .then(() => {
+                if (password) {
+                    bcrypt.hash(password, 10)
+                        .then(hashedPassword => {
+
+                            const user = new UserModel({
+                                username,
+                                password: hashedPassword,
+                                profile: profile || '',
+                                email
+                            });
+
+                            // return save result as a response
+                            user.save()
+                                .then(result => res.status(201).send({ msg: "User Register Successfully" }))
+                                .catch(error => res.status(500).send({ error }))
+
+                        }).catch(error => {
+                            return res.status(500).send({
+                                error: "Enable to hashed password"
+                            })
+                        })
+                }
+            }).catch(error => {
+                return res.status(500).send(error.message)
+            })
+
+    } catch (error) {
+        return res.status(500).send(error);
+    }
 }
 
 /** POST: http://localhost:8080/api/login 
@@ -23,12 +77,12 @@ export async function register(req,res){
   "password" : "admin123"
 }
 */
-export async function login(req,res){
+export async function login(req, res) {
     res.json('login route');
 }
 
 /** GET: http://localhost:8080/api/user/example123 */
-export async function getUser(req,res){
+export async function getUser(req, res) {
     res.json('getUser route');
 }
 
@@ -42,29 +96,29 @@ body: {
     profile : ''
 }
 */
-export async function updateUser(req,res){
+export async function updateUser(req, res) {
     res.json('updateUser route');
 }
 
 /** GET: http://localhost:8080/api/generateOTP */
-export async function generateOTP(req,res){
+export async function generateOTP(req, res) {
     res.json('generateOTP route');
 }
 
 /** GET: http://localhost:8080/api/verifyOTP */
-export async function verifyOTP(req,res){
+export async function verifyOTP(req, res) {
     res.json('verifyOTP route');
 }
 
 // successfully redirect user when OTP is valid
 /** GET: http://localhost:8080/api/createResetSession */
-export async function createResetSession(req,res){
+export async function createResetSession(req, res) {
     res.json('createResetSession route');
 }
 
 // update the password when we have valid session
 /** PUT: http://localhost:8080/api/resetPassword */
-export async function resetPassword(req,res){
+export async function resetPassword(req, res) {
     res.json('resetPassword route');
 }
 
