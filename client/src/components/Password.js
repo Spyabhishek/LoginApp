920016c58f245d2e3175c97344c4b4bbab8d12bd
kabhishek2023@gmail.com <kabhishek2023@gmail.com>
2023-03-18 11:21:05 +0530

    made some changes with the help of custom hooks

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -5,23 +5,31 @@ import avatar from '../assests/profile.png';
 import { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { passwordValidate } from '../helper/validate';
+import useFetch from '../hooks/fetch.hook';
+import { useAuthStore } from '../store/store.js';
 
 import styles from '../styles/Username.module.css';
 
 export default function Password() {
 
+  const { username } = useAuthStore(state => state.auth)
+  const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`)
+
   const formik = useFormik({
     initialValues: {
-      password: 'admin@123'
+      password: ''
     },
-    
+
     validate: passwordValidate,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async values => {
-      console.log(values)
+      // console.log(values)
     }
   })
+
+  if (isLoading) return <h1 className='text-2xl font-bold'>isLoading</h1>
+  if (serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1>
 
   return (
     <div className='container mx-auto'>
@@ -32,7 +40,7 @@ export default function Password() {
         <div className={styles.glass}>
 
           <div className='title flex flex-col items-center'>
-            <h4 className='text-5xl font-bold'>Hello Again!!</h4>
+            <h4 className='text-5xl font-bold'>Hello {apiData?.firstname || apiData?.username}</h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
               Explore More by connecting with us.
             </span>
@@ -40,7 +48,7 @@ export default function Password() {
 
           <form className='py-1' onSubmit={formik.handleSubmit}>
             <div className='profile flex justify-center py-4'>
-              <img src={avatar} className={styles.profile_img} alt="avatar" />
+              <img src={apiData?.profile || avatar} className={styles.profile_img} alt="avatar" />
             </div>
 
             <div className='textbox flex flex-col items-center gap-6'>
