d3ccc7a6d8cea1b22ad75da1479159f1718e2fe3
kabhishek2023@gmail.com <kabhishek2023@gmail.com>
2023-03-19 08:34:58 +0530

    completed the register component

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,17 +1,18 @@
 
-import React, {useState} from 'react';
-import { Link,  } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import avatar from '../assests/profile.png';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { registerValidation } from '../helper/validate';
 import convertToBase64 from '../helper/convert';
+import { registerUser } from '../helper/helper';
 
 import styles from '../styles/Username.module.css';
 
 export default function Register() {
 
-  // const navigate = useNavigate()
+  const navigate = useNavigate()
   const [file, setFile] = useState()
 
   const formik = useFormik({
@@ -24,13 +25,20 @@ export default function Register() {
     validate: registerValidation,
     validateOnBlur: false,
     validateOnChange: false,
-    onSubmit : async values => {
-      values = await Object.assign(values, { profile : file || ''})
+    onSubmit: async values => {
+      values = await Object.assign(values, { profile: file || '' })
+      let registerPromise = registerUser(values);
+      toast.promise(registerPromise, {
+        loading: 'Creating...',
+        success: <b>Register Successfully</b>,
+        error: <b>Could Not Register</b>
+      });
+      registerPromise.then(function () { navigate('/') });
     }
   })
 
-   /** formik doensn't support file upload so we need to create this handler */
-   const onUpload = async e => {
+  /** formik doensn't support file upload so we need to create this handler */
+  const onUpload = async e => {
     const base64 = await convertToBase64(e.target.files[0]);
     setFile(base64);
   }
@@ -41,7 +49,7 @@ export default function Register() {
       <Toaster position='top-center' reverseOrder={false}></Toaster>
 
       <div className='flex justify-center items-center h-screen'>
-        <div className={styles.glass} style={{width:'45%', paddingTop:'2em'}} >
+        <div className={styles.glass} style={{ width: '45%', paddingTop: '2em' }} >
 
           <div className='title flex flex-col items-center'>
             <h4 className='text-5xl font-bold'>Register</h4>
